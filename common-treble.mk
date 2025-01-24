a79b6943f609ee2e3d41531a53823a93b95e4739
Pavel Dubrova <pashadubrova@gmail.com>
2025-01-24 11:03:29 +0200

    Revert "snap"
    This reverts commit 01ed29885f51362e789c82fa360ddf945cca9da5.

--- common-treble.mk
+++ common-treble.mk
@@ -59,7 +59,8 @@ endif
 
 # External Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider-V1-external-service
+    android.hardware.camera.provider@2.5-external \
+    android.hardware.camera.provider@2.5-external-service
 
 # Media
 PRODUCT_PACKAGES += \
--- rootdir/vendor/bin/init.qcom.adspstart.sh
+++ rootdir/vendor/bin/init.qcom.adspstart.sh
@@ -1,35 +1,4 @@
 #!/vendor/bin/sh
 
 echo 1 > /sys/kernel/boot_adsp/boot
-
-MAX_RETRIES=5
-attempt=0
-
-# Check for the 'remoteproc' entry with 'adsp' in its name
-remoteproc_adsp=""
-
-# Check each remoteproc entry to see if the 'name' contains 'adsp'
-for proc in /sys/class/remoteproc/remoteproc*; do
-    if [[ -f "$proc/name" && $(cat "$proc/name") == *"adsp"* ]]; then
-        remoteproc_adsp=$(basename "$proc")
-        break
-    fi
-done
-
-while (( attempt < MAX_RETRIES )); do
-    remoteproc_adsp_state=$(cat /sys/class/remoteproc/$remoteproc_adsp/state 2>/dev/null || echo "UNKNOWN")
-    subsys_adsp_state=$(cat /sys/class/subsys/subsys_adsp/device/subsys*/state 2>/dev/null || echo "UNKNOWN")
-
-    if [[ "$subsys_adsp_state" == "ONLINE" || "$remoteproc_adsp_state" == "running" ]]; then
-        setprop vendor.qcom.adspup 1
-        log -t "$(basename $0)" "ADSP is ready!"
-        exit 0
-    fi
-
-    log -t "$(basename $0)" "ADSP is not ready yet. Retrying..."
-    ((attempt++))
-    sleep 2
-done
-
-log -t "$(basename $0)" "ADSP is not booted after $MAX_RETRIES retries."
-exit 1
+setprop vendor.qcom.adspup 1
